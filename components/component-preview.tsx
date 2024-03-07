9b7cd174eac080394af9fc3c89593bfaa63bf243
David Witt <midknight55@gmail.com>
2024-03-07 10:30:22 -0800

    Setup rough version of custom glove cursor in preview area

--- components/component-preview.tsx
+++ components/component-preview.tsx
@@ -2,10 +2,20 @@
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { getComponent } from "@/registry/utils";
-import { Suspense, useMemo, Children, HTMLAttributes, useState } from "react";
+import {
+	Suspense,
+	useMemo,
+	Children,
+	HTMLAttributes,
+	useState,
+	useRef,
+	LegacyRef,
+} from "react";
 import { Toggle } from "@/components/ui/toggle";
 import { cn } from "@/lib/utils";
-import { Inspect } from "lucide-react";
+import { Inspect, Hand } from "lucide-react";
+import { useMouse } from "@uidotdev/usehooks";
+import { clamp } from "@/utils/math";
 
 interface ComponentPreviewProps extends HTMLAttributes<HTMLDivElement> {
 	name: string;
@@ -50,9 +60,16 @@ export default function ComponentPreview({
 
 function Preview({ name }: { name: string }) {
 	const [debugActive, setDebugActive] = useState(false);
+	const [handActive, setHandActive] = useState(false);
+	const [mouse, ref] = useMouse();
+	const parentRef = useRef<HTMLDivElement>(null);
 
 	function toggleDebug() {
 		setDebugActive(!debugActive);
+	}
+
+	function toggleHand() {
+		setHandActive(!handActive);
 	}
 
 	const Preview = useMemo(() => {
@@ -76,19 +93,64 @@ function Preview({ name }: { name: string }) {
 
 	return (
 		<div
-			className={cn(
-				"p-4 flex items-center justify-center min-h-[340px] border rounded-md relative",
-				debugActive && "debug-active"
-			)}
+			className={cn(handActive && "custom-cursor-active")}
+			ref={parentRef}
+			onMouseLeave={() => setHandActive(false)}
 		>
-			<Suspense fallback={<div>Loading...</div>}>{Preview}</Suspense>
-			<Toggle
-				onClick={toggleDebug}
-				aria-label="toggle debug"
-				className="absolute bottom-2 left-2"
+			<div
+				ref={ref as LegacyRef<HTMLDivElement> | undefined}
+				className={cn(
+					"p-4 flex items-center justify-center min-h-[340px] border rounded-md relative",
+					debugActive && "debug-active"
+				)}
 			>
-				<Inspect className="h-4 w-4" />
-			</Toggle>
+				<Suspense fallback={<div>Loading...</div>}>{Preview}</Suspense>
+				<div className="absolute bottom-2 left-2 flex flex-col gap-2">
+					<Toggle onClick={toggleHand} aria-label="toggle debug">
+						{/* TODO replace with custom glove icon */}
+						<Hand className="h-4 w-4" />
+					</Toggle>
+					<Toggle onClick={toggleDebug} aria-label="toggle debug">
+						<Inspect className="h-4 w-4" />
+					</Toggle>
+				</div>
+				{handActive && (
+					<CustomCursor
+						{...mouse}
+						parentWidth={parentRef?.current?.offsetWidth ?? 0}
+						parentHeight={parentRef?.current?.offsetHeight ?? 0}
+					/>
+				)}
+			</div>
+		</div>
+	);
+}
+
+type CustomCursorProps = {
+	x: number;
+	y: number;
+	elementX: number;
+	elementY: number;
+	elementPositionX: number;
+	elementPositionY: number;
+	parentWidth: number;
+	parentHeight: number;
+};
+
+function CustomCursor({
+	elementX,
+	elementY,
+	parentWidth,
+	parentHeight,
+}: CustomCursorProps) {
+	const topPos = clamp(elementY, 0, parentHeight - 32);
+	const leftPos = clamp(elementX, 0, parentWidth - 32);
+	return (
+		<div
+			className="test-cursor"
+			style={{ position: "absolute", top: topPos, left: leftPos }}
+		>
+			<Hand className="w-8 h-8 -ml-4 -mt-4 pointer-events-none" />
 		</div>
 	);
 }
